b80bff596d88b71765ce19af4ef60d9fd094198a
Allen Ussher <allen@ussher.ca>
2018-05-23 18:50:31 -0700

    Initial Commit

---
+++ InfiniteScroll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InfiniteScroll
+//
+//  Created by Allen Ussher on 5/23/18.
+//  Copyright © 2018 Ussher Press. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
